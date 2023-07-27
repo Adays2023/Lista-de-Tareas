99d166b2008dfc9cb078423ab5455d023fe0511a
Adays2023 <granadosa@fwdcostarica.com>
2023-07-27 15:47:15 -0600

    lista

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-const empty = document.querySelector(".empty");
+const button = document.getElementById(".button");
+const counter = document.getElementById("counter");
+
+let contador = 0;
+
+inputBox.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+       addTask();
+    }
+})
 
 function addTask(){
     if(inputBox.value ===''){
@@ -9,35 +18,79 @@ function addTask(){
             title: 'Oops...',
             text: 'No Ingreso Tarea!',
             footer: '<a href="">Why do I have this issue?</a>'
-          })
-    }
+        })
+  
+     }      
     else{
-        let li = document.createElement("li");
         let inputbox = document.createElement("input");
+        let li = document.createElement("li");
         li.innerHTML = inputBox.value;
 
         const div = document.createElement("div")
+        div.id = 'ada'
         let input = document.createElement("input");
+
+        const deletebutton = document.createElement("button")
+        deletebutton.textContent = 'X'
+        deletebutton.className='deleteBtn'
+
         div.className = 'PruebaMarco'
+
         input.type = 'checkbox';
 
         div.appendChild(input)
         div.appendChild(li)
+        div.appendChild(deletebutton)
 
         listContainer.appendChild(div)
-        
 
-    }
-    inputBox.value = ''
-} 
+        input.addEventListener('click', function(){
+            if(input.checked){
+                console.log('esta checkeado')
+                contador++;
+                counter.textContent = contador
+            }
+            else{
+                console.log('No esta checkeado')
+                contador--;
+                counter.textContent = contador
+               
+            }
+        })
 
+        deletebutton.addEventListener('click', function(){
+            console.log('elimina')
+            contador--;
+            counter.textContent = contador
 
-
-// document.querySelector('').addEventListener('click',()=>{
-//     let p = document.getElementById('counter')
-//     let val = parseInt(p.textContent)
-//     val++
-//     p.textContent=val
-// })
+            div.remove();
+            saveDate();
+        })
 
     
+    }
+    inputBox.value = ''
+    saveDate();
+} 
+
+// funcion de guardado de datos 
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveDate();
+    }
+    } 
+,false);
+function saveDate(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+function imprime(){
+    let p = document.getElementById('counter')
+    p.textContent=contador
+}
